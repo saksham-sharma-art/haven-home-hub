f62bddbabbd5b193e073ce4b425494870734e2ae
saksham-sharma-art <sharmashaksham4301@gmail.com>
2026-06-01 15:41:02 +0530

    Add SEO metadata using Helmet component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { Helmet } from "react-helmet-async";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import FacilitiesSection from "@/components/FacilitiesSection";
@@ -13,6 +14,13 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
+      <Helmet>
+  <title>Cozy Heaven Girls PG & Hostel Near GLA University Mathura</title>
+  <meta name="description" content="Best girls PG and hostel near GLA University, Mathura. Safe, affordable, home-style meals, WiFi & 24/7 security. Call +91-8679691985." />
+  <link rel="canonical" href="https://www.thecozyheaven.com/" />
+  <meta property="og:title" content="Cozy Heaven Girls PG & Hostel Near GLA University Mathura" />
+  <meta property="og:url" content="https://www.thecozyheaven.com/" />
+</Helmet>
       <Navbar />
       <HeroSection />
       <AboutSection />
